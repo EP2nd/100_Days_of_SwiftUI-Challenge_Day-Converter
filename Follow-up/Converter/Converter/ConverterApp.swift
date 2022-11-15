68c91b25071e15035295f42dbd617e366072cc36
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-11-15 11:58:12 +0100

    Improvement
    Exchanged conditional statements and functions with two switch statements. The code is significantly shorter.

---
+++ Follow-up/Converter/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Paul Hudson on 18/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
